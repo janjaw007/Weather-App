dd34ca7731c218f7f96a374a3f3d023ca767e102
janjaw007 <chayanit.kitm@gmail.com>
2025-07-31 08:36:39 +0700

    getcurrent position and display on ui

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("hello weather app");
+
+const titleElement = document.querySelector(".content-title");
+const latElement = document.querySelector(".content-lat");
+const lngElement = document.querySelector(".content-lng");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    titleElement.textContent = "Loading Location...";
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function success(position) {
+  const latPos = position.coords.latitude;
+  const lngPos = position.coords.longitude;
+
+  const latPosRounded = latPos.toFixed(3);
+  const lngPosRounded = lngPos.toFixed(3);
+  const titleDisplay = `your location is lat:${latPosRounded} lng:${lngPosRounded}`;
+
+  titleElement.textContent = titleDisplay;
+}
+
+function error() {
+  alert("Sorry, No position Available");
+}
+getLocation();
